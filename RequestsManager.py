1499963f701741975d9ef4f22dfa2e3451b3e868
Covac <filip.pazitka@gmail.com>
2024-02-17 04:25:15 +0100

    Decided on requests.request

--- RequestsManager.py
+++ RequestsManager.py
@@ -1,4 +1,8 @@
 import requests
+from globals import proxyLink
+from urllib3 import disable_warnings
+
+disable_warnings()
 
 class RequestsManager:
     def __init__(self):
@@ -8,11 +12,21 @@ class RequestsManager:
         #prepared proxy lists, ready to use with requests
         self.ppl = []
         self.uppl = []
+        
+    def clearLists(self):
+        self.proxy_list.clear()
+        self.unique_proxy_list.clear()
+        self.ppl.clear()
+        self.uppl.clear()
 
     def refreshProxyList(self):
         #maybe use options for more params
-        pr = requests.get()
+        if len(self.ppl) > 0 or len(self.uppl) > 0:
+        	self.clearLists()
+        pr = requests.get(proxyLink)
         self.proxy_list = [x.replace("socks5","socks5h") for x in pr.text.split('\r\n') if len(x)>1]
+        self.generateUniqueProxyList()
+        self.prepareProxies()
 
     def generateUniqueProxyList(self):
         for p in self.proxy_list:
@@ -26,12 +40,15 @@ class RequestsManager:
                     save = False
                     break
             if save:
-                self.unique_proxy_list(p)
+                self.unique_proxy_list.append(p)
 
     def prepareProxies(self):
         self.ppl = [{'http':p, 'https':p} for p in self.proxy_list]
         self.uppl = [{'http':p, 'https':p} for p in self.unique_proxy_list]
 
-    #Think about maybe just requests.request vs making 2 methods?!?
-    def getWithProxy(self,session,unique=False,params=None,json=None,**kwargs):
-        #done for now!
+    def requestWithProxy(self,method,url,session,unique=False,**kwargs):
+        kwargs['verify'] = False
+        kwargs['proxies'] = self.uppl.pop(0) if unique else self.ppl.pop(0)
+        r = session.request(method,url,**kwargs)
+        print(kwargs)
+        return r
